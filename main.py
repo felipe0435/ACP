e882cfef57d7b07ce7c6b87c6b345cb02259c7ac
felipe0435 <felipeantoniomendezcares@gmail.com>
2023-12-07 17:03:31 -0300

    Changes to be committed: 	renamed:    PDB_31-12-2012.csv -> PDB.csv 	modified:   main.py 	modified:   proteina.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import csv
+from proteina import Proteina
+
+
+
+
+
+def main():
+    proteinas = []
+    with open("PDB.csv") as tsvF:
+        for i in range(5878):
+            reader = csv.reader(tsvF)
+            linea = next(reader)
+            if i > 0:
+                proteina = Proteina(linea[1], linea[2], linea[3])
+                proteina.set_largo_secuencia()
+                proteina.set_cant_est_alfa()
+                proteina.set_cant_est_beta()
+                proteinas.append(proteina)
+    for i in range(5877):
+        print(proteinas[i].get_cant_estructuras())
+
+
+if __name__ == '__main__':
+    main()
--- proteina.py
+++ proteina.py
@@ -1,80 +1,111 @@
 class Proteina(object):
-    def __init__(self):
+    def __init__(self, codigo, secuencia, estructura):
         # datos
-        self.codigo = None
-        self.cadena = None
+        self.codigo = codigo
+        self.secuencia = secuencia
         self.largo_secuencia = 0
+        self.secuencia_est = estructura
         # secuencias
         self.secuencia_aa = None
-        self.secuencia_est = None
         # Cantidad de estructuras secuendarias
         self.cant_est_alfa = 0
         self.cant_est_beta = 0
         # Frecuencias
-        self.aa_mas_frecuente = None
-        self.est_dominante = None
-        
+        self.aa_mas_frecuente = 0
+        self.est_dominante = 0
+
         """
         self.peso_molecular = 0
         se calcularia multiplicando por un valor fijo cada aminoacido (puede ir
         se a la mierda si es que lo hacemos con los 5900)
         """
-        
-        # Get y Set
-        def get_proteina(self):
-            return self.codigo, self.cadena
-        
-        def get_largo_secuencia(self):
-            return self.largo_secuencia
-        
-        def get_secuencia_aa(self):
-            return self.secuencia_aa
-        
-        def get_secuencia_est(self):
-            return self.secuencia_est
-        
-        def get_cant_estructuras(self):
-            return self.cant_est_alfa, self.cant_est_beta
-        
-        def get_aa_mas_frecuente(self):
-            return self.aa_mas_frecuente
-        
-        def get_estructura_frecuente(self):
-            return self.est_dominante
-        
-        def set_codigo(self, codigo):
-            self.codigo = codigo
-        
-        def set_cadena(self, cadena):
-            self.cadena = cadena
-        
-        def set_largo_secuencia(self, largo):
-            self.largo_secuencia = largo
-        
-        def set_secuencia_aa(self, secuencia):
-            self.secuencia_aa = secuencia
-        
-        def set_secuencia_est(self, secuencia):
-            self.secuencia_est = secuencia
-        
-        def set_cant_est_alfa(self, cantidad):
-            self.cant_est_alfa = cantidad
-            if self.cant_est_beta != 0:
-                set_est_dominante()
-        
-        def set_cant_est_beta(self, cantidad):
-            self.cant_est_beta = cantidad
-            if self.cant_est_alfa != 0:
-                set_est_dominante()
-        
-        """
-        self.aa_mas_frecuente = None
-        """
-        
-        def set_est_dominante(self):
-            if self.cant_est_alfa > self.cant_est_beta:
-                self.est_dominante = "Alfa"
-            if self.cant_est_beta > self.cant_est_alfa:
-                self.est_dominante = "Beta"
-            else:
-                self.est_dominante = "Ambas"
+
+
+    # Get y Set
+    def set_codigo(self, codigo):
+        self.codigo = codigo
+
+
+    def set_largo_secuencia(self):
+        largo = len(self.secuencia)
+        self.largo_secuencia = largo
+
+
+    def set_secuencia(self, secuencia):
+        self.secuencia = secuencia
+
+
+    def set_secuencia_est(self, secuencia):
+        self.secuencia_est = secuencia
+
+
+    def set_cant_est_alfa(self):
+        cantidad = 0
+        alfa = False
+        for letra in self.secuencia_est:
+            if letra == "H" and not alfa:
+                cantidad += 1
+                alfa = True
+            elif letra != "H" and alfa:
+                alfa = False
+        self.cant_est_alfa = cantidad
+        self.set_est_dominante()
+
+
+    def set_cant_est_beta(self):
+        cantidad = 0
+        beta = False
+        for letra in self.secuencia_est:
+            if letra == "E" and not beta:
+                cantidad += 1
+                beta = True
+            elif letra != "E" and beta:
+                beta = False
+        self.cant_est_beta = cantidad
+        self.set_est_dominante()
+
+
+    def set_est_dominante(self):
+        if self.cant_est_alfa > self.cant_est_beta:
+            self.est_dominante = "Alfa"
+        if self.cant_est_beta > self.cant_est_alfa:
+            self.est_dominante = "Beta"
+        elif self.cant_est_alfa == self.cant_est_beta:
+            self.est_dominante = "Ambas"
+
+
+    def get_codigo(self):
+        return self.codigo
+
+
+    def get_secuencia(self):
+        return self.secuencia
+
+
+    def get_largo_secuencia(self):
+        return self.largo_secuencia
+
+
+    def get_secuencia_aa(self):
+        return self.secuencia_aa
+
+
+    def get_secuencia_est(self):
+        return self.secuencia_est
+
+
+    def get_cant_estructuras(self):
+        return self.cant_est_alfa, self.cant_est_beta
+
+
+    def get_aa_mas_frecuente(self):
+        return self.aa_mas_frecuente
+
+
+    def get_estructura_dominante(self):
+        return self.est_dominante
+
+
+    """
+    self.aa_mas_frecuente = None
+    """
